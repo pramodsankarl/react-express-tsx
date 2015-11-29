7192b3a7b1b6eb23e59bcae2d2181c78418a0531
PSL <pramodsankar.l@gmail.com>
2015-11-29 18:33:52 -0500

    More clean up

--- server/main.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) { return res.render('./../app/index.ejs', {}); })
-    .use(express.static(__dirname + '/../.tmp'))
-    .listen(7777);
